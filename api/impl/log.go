79abc288a2fdd0daee04a103a5236baa2ca8d829
frrist <forrest@protocol.ai>
2018-09-26 09:37:06 -0700

    add `log stream` command
    - accepts a multiaddress to stream event logs to
    - fixes #959

--- api/impl/log.go
+++ api/impl/log.go
@@ -1,11 +1,20 @@
 package impl
 
 import (
+	"bufio"
 	"context"
 	"io"
+	"time"
 
+	logging "gx/ipfs/QmRREK2CAZ5Re2Bd9zZFG6FeYDppUWt5cMgsoUEp3ktgSr/go-log"
 	writer "gx/ipfs/QmRREK2CAZ5Re2Bd9zZFG6FeYDppUWt5cMgsoUEp3ktgSr/go-log/writer"
+	manet "gx/ipfs/QmV6FjemM1K8oXjrvuq3wuVWWoU2TLDPmNnKrxHzY3v6Ai/go-multiaddr-net"
+	ma "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
 )
+
+var log = logging.Logger("api/impl")
+var LogStreamJoinEvent = "LogStreamJoin"
+var LogStreamLeaveEvent = "LogStreamLeave"
 
 type nodeLog struct {
 	api *nodeAPI
@@ -25,4 +34,44 @@ func (api *nodeLog) Tail(ctx context.Context) io.Reader {
 	writer.WriterGroup.AddWriter(w)
 
 	return r
+}
+
+func (api *nodeLog) Stream(ctx context.Context, maddr ma.Multiaddr) error {
+	nodeDetails, err := api.api.ID().Details()
+	if err != nil {
+		return err
+	}
+
+	peerID := nodeDetails.ID
+	mconn, err := manet.Dial(maddr)
+	if err != nil {
+		return err
+	}
+
+	r, w := io.Pipe()
+	go func() {
+		// node leaves a connection
+		defer w.Close() // nolint: errcheck
+		defer r.Close()
+		<-ctx.Done()
+		ctx = log.Start(ctx, LogStreamLeaveEvent)
+		log.SetTag(ctx, "peerID", peerID)
+		log.Finish(ctx)
+		time.Sleep(2 * time.Second)
+	}()
+
+	writer.WriterGroup.AddWriter(w)
+	wconn := bufio.NewWriter(mconn)
+
+	// node joins a connection
+	ctx = log.Start(ctx, LogStreamJoinEvent)
+	log.SetTag(ctx, "peerID", peerID)
+	log.Finish(ctx)
+
+	_, err = wconn.ReadFrom(r)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- commands/log.go
+++ commands/log.go
@@ -3,6 +3,8 @@ package commands
 import (
 	cmds "gx/ipfs/QmPTfgFTo9PFr1PvPKyKoeMgBvYPh6cX3aDP7DHKVbnCbi/go-ipfs-cmds"
 	cmdkit "gx/ipfs/QmSP88ryZkHSRn1fnngAaV2Vcn63WUJzAavnRM9CVdU1Ky/go-ipfs-cmdkit"
+
+	ma "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
 )
 
 var logCmd = &cmds.Command{
@@ -15,7 +17,8 @@ output of a running daemon.
 	},
 
 	Subcommands: map[string]*cmds.Command{
-		"tail": logTailCmd,
+		"tail":   logTailCmd,
+		"stream": logStreamCmd,
 	},
 }
 
@@ -30,5 +33,29 @@ Outputs event log messages (not other log messages) as they are generated.
 	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) {
 		r := GetAPI(env).Log().Tail(req.Context)
 		re.Emit(r) // nolint: errcheck
+	},
+}
+
+var logStreamCmd = &cmds.Command{
+	Helptext: cmdkit.HelpText{
+		Tagline: "Stream the event logs to a multiaddress.",
+		ShortDescription: `
+Outputs event log messages (not other log messages) as they are generated to
+ the specified multiaddress
+`,
+	},
+	Arguments: []cmdkit.Argument{
+		cmdkit.StringArg("addr", true, false, "multiaddress logs will stream to"),
+	},
+	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) {
+		maddr, err := ma.NewMultiaddr(req.Arguments[0])
+		if err != nil {
+			re.SetError(err, cmdkit.ErrNormal)
+			return
+		}
+		if err := GetAPI(env).Log().Stream(req.Context, maddr); err != nil {
+			re.SetError(err, cmdkit.ErrNormal)
+			return
+		}
 	},
 }
