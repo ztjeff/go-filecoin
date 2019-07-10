6dbfef3c9de0c35256700d5d4aa6e22cdc8f4829
frrist <forrest@protocol.ai>
2019-07-10 13:23:02 -0700

    add sketch of peer manager interface and impl

---
+++ chain/peer_manager.go
@@ -0,0 +1,55 @@
+package chain
+
+import (
+	"fmt"
+	"sync"
+
+	peer "github.com/libp2p/go-libp2p-peer"
+
+	"github.com/filecoin-project/go-filecoin/types"
+)
+
+type PeerManager interface {
+	// AddPeer adds a peer to the peer managers store.
+	AddPeer(p peer.ID, h types.TipSet)
+
+	// PeerHeads returns all peers and their heads the peer manager knows about.
+	PeerHeads() map[peer.ID]types.TipSet
+
+	// Select computes the best head from the peers its aware of.
+	SelectHead() (types.TipSet, error)
+}
+
+type BasicPeerManager struct {
+	peerHeadMu sync.Mutex
+	peerHeads  map[peer.ID]types.TipSet
+}
+
+func NewBasicPeerManager() *BasicPeerManager {
+	return &BasicPeerManager{
+		peerHeads: make(map[peer.ID]types.TipSet),
+	}
+}
+
+func (bpm *BasicPeerManager) AddPeer(p peer.ID, h types.TipSet) {
+	bpm.peerHeadMu.Lock()
+	defer bpm.peerHeadMu.Unlock()
+	bpm.peerHeads[p] = h
+}
+
+func (bpm *BasicPeerManager) PeerHeads() map[peer.ID]types.TipSet {
+	bpm.peerHeadMu.Lock()
+	defer bpm.peerHeadMu.Unlock()
+	return bpm.peerHeads
+}
+
+func (bpm *BasicPeerManager) SelectHead() (types.TipSet, error) {
+	if len(bpm.peerHeads) == 0 {
+		return types.UndefTipSet, fmt.Errorf("zero peers to select from")
+	}
+	// for now pick something random?
+	for _, h := range bpm.peerHeads {
+		return h, nil
+	}
+	panic("unreachable")
+}
