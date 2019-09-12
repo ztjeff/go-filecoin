875e8abe4702dafa956380d2d8e0febff151484c
Travis Person <travisperson@users.noreply.github.com>
2019-09-11 18:21:04 -0700

    Add peerkey tool

--- tools/peerkey/main.go
+++ tools/peerkey/main.go
@@ -1,28 +1,44 @@
 package main
 
+// peerkey will generate an RSA 2048 peerkey, writing it to the output specified by
+// the -output flag. Instance of `<peerid>` will be replaced by the peerid in the
+// output filename.
+
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/libp2p/go-libp2p-crypto"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func main() {
-	var output string = "peer.key"
+	var output string = "<peerid>.peerkey"
+	var silent bool = false
 
-	flag.StringVar(&output, "o", output, "file to write peerkey too")
+	flag.StringVar(&output, "output", output, "output of peerkey file")
+	flag.BoolVar(&silent, "silent", silent, "suppress output of peerid to stdout")
 	flag.Parse()
 
-	pk, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+	sk, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bs, err := crypto.MarshalPrivateKey(pk)
+	bs, err := crypto.MarshalPrivateKey(sk)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	peerid, err := peer.IDFromPrivateKey(sk)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output = strings.ReplaceAll(output, "<peerid>", peerid.String())
 
 	f, err := os.Create(output)
 	if err != nil {
@@ -37,5 +53,9 @@ func main() {
 
 	if _, err := f.Write(bs); err != nil {
 		log.Fatal(err)
+	}
+
+	if !silent {
+		fmt.Println(peerid.String())
 	}
 }
