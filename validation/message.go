9cd8da1329c2953fa91daa912062405699df33a2
anorth <anorth@users.noreply.github.com>
2019-10-14 16:55:58 +1100

    wip, working message factory

---
+++ validation/message.go
@@ -0,0 +1,32 @@
+package validation
+
+import (
+	"github.com/filecoin-project/chain-validation/pkg/chain"
+	"github.com/filecoin-project/chain-validation/pkg/state"
+
+	"github.com/filecoin-project/go-filecoin/abi"
+	"github.com/filecoin-project/go-filecoin/address"
+	"github.com/filecoin-project/go-filecoin/types"
+)
+
+type MessageFactory struct {
+}
+
+var _ chain.MessageFactory = &MessageFactory{}
+
+func (mf *MessageFactory) MakeMessage(from, to state.Address, method state.MethodID, nonce uint64, value state.AttoFIL, params ...interface{}) (interface{}, error) {
+	fromDec, err := address.NewFromBytes([]byte(from))
+	if err != nil {
+		return nil, err
+	}
+	toDec, err := address.NewFromBytes([]byte(to))
+	if err != nil {
+		return nil, err
+	}
+	valueDec := types.NewAttoFIL(value)
+	paramsDec, err := abi.ToEncodedValues(params)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewMessage(fromDec, toDec, nonce, valueDec, string(method), paramsDec), nil
+}
---
+++ validation/message_test.go
@@ -0,0 +1,30 @@
+package validation
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/filecoin-project/chain-validation/pkg/chain"
+	"github.com/filecoin-project/chain-validation/pkg/state"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-filecoin/address"
+	"github.com/filecoin-project/go-filecoin/types"
+)
+
+func TestMessageFactory(t *testing.T) {
+	factory := &MessageFactory{}
+	p := chain.NewMessageProducer(factory)
+
+	require.NoError(t, p.Transfer(state.NetworkAddress, state.BurntFundsAddress, big.NewInt(1)))
+
+	messages := p.Messages()
+	assert.Equal(t,1, len(messages))
+	msg := messages[0].(*types.Message)
+	assert.Equal(t, address.NetworkAddress, msg.From)
+	assert.Equal(t, address.BurntFundsAddress, msg.To)
+	assert.Equal(t, types.NewAttoFIL(big.NewInt(1)), msg.Value)
+}
+
+
