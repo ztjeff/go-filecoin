8ef88838c22b1b7fabb8918285d29fb3d347e5c7
frrist <forrest@protocol.ai>
2019-10-16 16:28:45 -0700

    handle message receipts

--- validation/example_test.go
+++ validation/example_test.go
@@ -4,11 +4,10 @@ import (
 	"testing"
 
 	vmtool "github.com/filecoin-project/chain-validation/pkg/validation"
-
 )
 
-func TestTryItOut(t *testing.T) {
+func TestExample(t *testing.T) {
 	stateFactory := NewStateFactory()
 	msgFactory := NewMessageFactory(stateFactory.Signer())
-	vmtool.TryItOut(t, msgFactory, stateFactory)
+	vmtool.Example(t, msgFactory, stateFactory)
 }
