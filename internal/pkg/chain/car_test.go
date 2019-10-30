ffbc88e18a222d596c19c3a013253dfc18fdc560
acruikshank <acruikshank@example.com>
2019-10-30 10:48:43 -0400

    graphsync no longer fetches receipts

--- internal/pkg/chain/car_test.go
+++ internal/pkg/chain/car_test.go
@@ -164,9 +164,8 @@ func TestChainImportExportMessages(t *testing.T) {
 		mm.NewSignedMessage(alice, 4),
 		mm.NewSignedMessage(alice, 5),
 	}
-	rcts := types.EmptyReceipts(5)
 	ts2 := cb.BuildOneOn(ts1, func(b *chain.BlockBuilder) {
-		b.AddMessages(msgs, []*types.UnsignedMessage{}, rcts)
+		b.AddMessages(msgs, []*types.UnsignedMessage{})
 	})
 
 	// export the car file to a buffer
@@ -197,12 +196,10 @@ func TestChainImportExportMultiTipSetWithMessages(t *testing.T) {
 		mm.NewSignedMessage(alice, 4),
 		mm.NewSignedMessage(alice, 5),
 	}
-	rcts := types.EmptyReceipts(5)
 	ts2 := cb.BuildOneOn(ts1, func(b *chain.BlockBuilder) {
 		b.AddMessages(
 			msgs,
 			[]*types.UnsignedMessage{},
-			rcts,
 		)
 	})
 
